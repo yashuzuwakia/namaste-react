3f7098228eaf0d31ee07713249141662b5d8a8ce
Yash <iam.007yash@gmail.com>
2025-01-30 00:54:25 +0530

    Added all the codes of react with CDN only

---
+++ App.js
@@ -0,0 +1,17 @@
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+
+
+
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
