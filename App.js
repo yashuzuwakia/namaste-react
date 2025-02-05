3c2ff5e01f8d5f02a6d44398c03f507e80fbd3eb
Yash <iam.007yash@gmail.com>
2025-02-05 22:33:57 +0530

    Added many component and apllied css to it

--- App.js
+++ App.js
@@ -1,36 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Code written by using pure React
-const heading = React.createElement("h1", { id: "heading" }, "I am React App");
+/**
+ * Header
+ *  -Logo
+ *  -Nav
+ * Body
+ *  -Restraunt Container
+ *    -Restraunt Card
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Contacts
+ *  -Address
+ */
 
-// JSX is HTML like or XML Like syntax
-// written by useing JSX
-//JSX is tranpiled before it reaches to JS by - Parcel - It is actully done by Babel
-const jsxHeading = (
-  <h1 id="heading" tabIndex="5">
-    This is React using JSX
-  </h1>
-);
-
-const Title = () => {
-  return <h1>I am the title of this app.</h1>;
-};
-
-function NormalFunction() {
-  return <h1>I am normal Function</h1>;
-}
-//React Functional Component
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div>
-      <Title />
-      <NormalFunction />
-      {jsxHeading}
-      <h1 className="heading">Namaste React Functional Component!! 🚀</h1>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
+      </div>
+      <div className="nav">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
+
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestrauntCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/de56dffcc29d6ad046285c5b664bda8f"
+      />
+      <h3>Meghna Foods</h3>
+      <h4>Biryani, Chinese, North Indian</h4>
+      <h4>4.4 stars</h4>
+      <h4>20 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
